0c1f00578c077c2d007486acf08ee3fce3f5fab6
Brian Ferris <bdferris+v2@google.com>
2024-08-30 19:21:15 -0700

    Update README and default export with new stringify method.  Also add support for custom XMLSerializer.

--- lib/stringify.ts
+++ lib/stringify.ts
@@ -1,18 +1,24 @@
 import { ParsedGPX } from "./parsed_gpx";
 import { Extensions } from "./types";
 
+interface XMLSerializationStrategy {
+    serializeToString(doc: Document): string;
+}
+
 /**
  * Converts a ParsedGPX object back into a GPX XML string.
  * @param gpx the parsed GPX object to serialize
+ * @param customXmlSerializer an optional custom XMLSerializer implementation.
+ *   If not specified, a default XMLSerializer instance will be created.
  * @returns a serialized XML string in the GPX format
  */
-export function stringifyGPX(gpx: ParsedGPX): string {
+export function stringifyGPX(gpx: ParsedGPX, customXmlSerializer?: XMLSerializationStrategy): string {
     const doc = gpx.xml.implementation.createDocument(GPX_NS, "gpx");
     doc.documentElement.setAttribute('version', '1.1');
     doc.documentElement.setAttribute('creator', 'gpxjs');
     const mapper = new XmlMapper(doc);
     mapper.mapObject(GPX_MAPPING, gpx, doc.documentElement);
-    const serializer = new XMLSerializer();
+    const serializer = customXmlSerializer ?? new XMLSerializer();
     return serializer.serializeToString(doc);
 }
 
@@ -270,7 +276,7 @@ class XmlMapper {
         if (value != null && typeof value === 'object' && fieldName === 'time') {
             return value.toISOString();
         }
-        
+
         return value;
     }
 }
